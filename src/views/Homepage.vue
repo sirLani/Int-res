d529c118e1b302a5ca3071bb6f7aef5b055cc3df
Bara Konateh <bkonateh1@gmail.com>
2022-03-11 09:30:06 +0000

    Project Init

---
+++ src/views/Homepage.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <h1> Welcome to your Technical Test!</h1>
+    </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style lang="scss" scoped>
+
+</style>
